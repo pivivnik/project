30085ef685cd68b0105fba8e244592b8946152b9
Test User <piviv64@gmail.com>
2018-04-13 18:25:47 +0300

    the creating Modal

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Quote } from '../../data/quote.interface';
 import { QuotesService } from '../../services/quotes';
+import { ModalController } from 'ionic-angular';
+import { QuotePage } from '../quote/quote';
+import { isPresent } from 'ionic-angular/util/util';
 
 
 @Component({
@@ -10,10 +13,16 @@ import { QuotesService } from '../../services/quotes';
 export class FavoritesPage {
     quotes: Quote[];
 
-    constructor (private quotesService: QuotesService) {}
+    constructor (private quotesService: QuotesService,
+                 private modalCtrl: ModalController) {}
 
     ionViewWillEnter(){
      this.quotes = this.quotesService.getFavoriteQuotes();
+    }
+
+    onViewQuote(quote: Quote) {
+      const modal = this.modalCtrl.create(QuotePage);
+      modal.present();
     }
 
 
