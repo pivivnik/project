77febe6300a5a03d10d23973d0d4024a0a38d274
Test User <piviv64@gmail.com>
2018-04-14 20:52:47 +0300

    passing the date (name of person, text of quote) to the Modal

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,25 +1,25 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the QuotePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+  person: string;
+  text: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor (private viewCtrl: ViewController,
+               private navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+    this.person = this.navParams.get('person');
+    this.text = this.navParams.get('text');
+  }
+
+  onClose() {
+    this.viewCtrl.dismiss();
   }
 
 }
