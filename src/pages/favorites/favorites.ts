09c9e6807aed219abe9b06a01722e3a07102f1d5
Test User <piviv64@gmail.com>
2018-04-26 14:52:36 +0300

    Recieving a sliding button

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -23,19 +23,21 @@ export class FavoritesPage {
     onViewQuote(quote: Quote) {
       const modal = this.modalCtrl.create(QuotePage, quote);
       modal.present();
-      
       modal.onDidDismiss((remove: boolean) => {
         if (remove) {
-          this.quotesService.removeQuoteFromFavorites(quote);
-         // this.quotes = this.quotesService.getFavoriteQuotes(); 
-         const position = this.quotes.findIndex((quoteEl: Quote) => {
-           return quoteEl.id == quote.id;
-         });
-         this.quotes.splice(position, 1);
+          this.onRemoveFromFavorites(quote);
         }
-      });
-      
+      });  
     }
+    onRemoveFromFavorites(quote: Quote) {
+      this.quotesService.removeQuoteFromFavorites(quote);
+      // this.quotes = this.quotesService.getFavoriteQuotes(); 
+      const position = this.quotes.findIndex((quoteEl: Quote) => {
+        return quoteEl.id == quote.id;
+      });
+      this.quotes.splice(position, 1);
+    }
+
   }
   
 
